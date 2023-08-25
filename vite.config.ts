b692048b26bebf5e63d4e2373c187085b31ae118
lucasNlind <lucaslind7@gmail.com>
2023-08-25 16:51:26 -0400

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ai-ui-template',
+  base: './ai-ui-template',
   plugins: [react()],
 })
